2fbb58f1102ce542e7508b04ef7a6be280272784
as0113-dev <52647014+as0113-dev@users.noreply.github.com>
2021-12-07 02:11:59 -0500

    Update unique.sql
    removed extra lines, and set up query locations to be added

--- unique.sql
+++ unique.sql
@@ -1,4 +1,5 @@
-									/*5 UNIQUE QUERIES*/
+								/*5 UNIQUE QUERIES*/
+/* to display tables for visuals when needed*/								
 select * from bought;
 select * from brands;
 select * from customer;
@@ -12,11 +13,15 @@ select * from store;
 select * from vendor;
 select * from visits;
 
-/*QUERY # 1, project top 3 most visited stores by customers*/
-
+/*QUERY # 1, project store_id of our top 3 most visited stores*/
 
 select store_id as top3_visited from (select count(cust_id), store_id from visits group by store_id order by count(cust_id) desc limit 3) as t;
 
-
-# QUERY #2, get the max and min weight of an item from each brand 
+/* QUERY #2, get the max and min weight of an item from each brand */
 SELECT brand_name, MAX(weight), MIN(weight) FROM in_stock NATURAL JOIN product NATURAL JOIN produces group by brand_name;
+
+/*QUERY # 3*/
+
+/*QUERY # 4*/
+
+/*QUERY # 5*/
