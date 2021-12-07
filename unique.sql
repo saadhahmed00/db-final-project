2d20fe2759b74cac111cd79beae09ccc05eb81f6
as0113-dev <52647014+as0113-dev@users.noreply.github.com>
2021-12-07 03:09:10 -0500

    Update unique.sql
    updated query number 3

--- unique.sql
+++ unique.sql
@@ -20,8 +20,9 @@ select store_id as top3_visited from (select count(cust_id), store_id from visit
 /* QUERY #2, get the max and min weight of an item from each brand */
 SELECT brand_name, MAX(weight), MIN(weight) FROM in_stock NATURAL JOIN product NATURAL JOIN produces group by brand_name;
 
-/*QUERY # 3, get the name and address of a customer who has bought an item with a specific UPC code*/
-SELECT NAME, address FROM customer NATURAL JOIN bought WHERE UPC = 430933048909;
+/*QUERY # 3, get the name,address, and phone number of a customer who has bought an item with a specific UPC code*/
+
+SELECT name, address, `phone number` FROM customer NATURAL JOIN bought WHERE UPC = 430933048909;
 
 /*QUERY # 4, get the store id, the address of the store, open and closing times of the store for a specific product at a specific state*/
 SELECT store_id,address,OPEN,close FROM product NATURAL JOIN in_stock NATURAL JOIN store WHERE state='Georgia'  AND NAME='sed pede.';
